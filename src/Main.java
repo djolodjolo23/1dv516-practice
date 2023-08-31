e8a07240328a3a0b0b434b8da3c8627c14ab40fb
Djordje Dimitrov <110050027+djolodjolo23@users.noreply.github.com>
2023-08-31 11:36:55 +0200

    Working on ex 1 from the chapter 1 in the book.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
+  public static void main(String[] args) {
+    int [] array = {24, 36, 48};
+
+    System.out.println(getFactors(array));
+
+  }
+  public static List<List<Integer>> getFactors(int[] arr) {
+    int primeNum = 2;
+    List<List<Integer>> outerList = new ArrayList<>();
+    for (int num : arr) {
+      List<Integer> factors = new ArrayList<>();
+      while (num % primeNum != 0) {
+        primeNum += 1;
+      }
+      int powerCount = 0;
+      while (num % primeNum == 0) {
+        num /= primeNum;
+        powerCount += 1;
+        // need to check when to raise the num to +1
+        if (num == primeNum) {
+          factors.add(powerCount);
+          outerList.add(factors);
+          break;
+        }
+      }
+    }
+
+    return outerList;
+  }
 }
---
+++ src/ex1.java
@@ -0,0 +1,13 @@
+public class ex1 {
+
+  public ex1() {
+  }
+
+  public int getHCF(int [] arr) {
+    int hcf = 0;
+    for (int i : arr) {
+
+    }
+    return hcf;
+  }
+}
