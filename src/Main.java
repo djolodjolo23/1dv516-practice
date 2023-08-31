34bc6c8fd5ee57c6745bf781366728f3addaa86a
Djordje <dd222im@student.lnu.se>
2023-08-31 12:15:34 +0200

    Exercise almost done, need to map the correct numbers to their powers. And to calculate the MSE

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,15 @@ import java.util.List;
 public class Main {
 
   public static void main(String[] args) {
-    int [] array = {24, 36, 48};
+    int [] array = {24, 36, 48, 5};
 
     System.out.println(getFactors(array));
 
   }
   public static List<List<Integer>> getFactors(int[] arr) {
-    int primeNum = 2;
     List<List<Integer>> outerList = new ArrayList<>();
     for (int num : arr) {
+      int primeNum = 2;
       List<Integer> factors = new ArrayList<>();
       while (num % primeNum != 0) {
         primeNum += 1;
@@ -22,7 +22,12 @@ public class Main {
         num /= primeNum;
         powerCount += 1;
         // need to check when to raise the num to +1
-        if (num == primeNum) {
+        if (num % primeNum == 1) {
+          primeNum += 1;
+          factors.add(powerCount);
+          powerCount = 0;
+        }
+        if (num == 1) {
           factors.add(powerCount);
           outerList.add(factors);
           break;
